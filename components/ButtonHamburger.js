fe8ca7222891f4eea9a4a8c508718efa94efc211
Jesus Rivera <jgrcreations@gmail.com>
2022-01-30 21:31:10 -0500

    adaptado a js modules y html custom

--- components/ButtonHamburger.js
+++ components/ButtonHamburger.js
@@ -1,3 +1,5 @@
+import './FloatOption.js'
+
 class ButtonHamburger extends HTMLElement {
   constructor () {
     super()
@@ -30,11 +32,55 @@ class ButtonHamburger extends HTMLElement {
       span.primer { transform: translate(-50%, calc(-50% - 7px)); }
 
       span.cuarto { transform: translate(-50%, calc(-50% + 7px)); }
+
+      span.segundo-mod, span.tercer-mod {
+        background-color: Red;
+        width: 1.6rem;
+        height: 3px;
+      }
+
+      span.primer-mod {
+        transform: translate(-50%, calc(-50% - 14px));
+        width: 1rem;
+        background-color: transparent;
+      }
+
+      span.cuarto-mod {
+        transform: translate(-50%, calc(-50% + 14px));
+        width: .5rem;
+        background-color: transparent;
+      }
+
+      span.segundo-mod { transform: translate(-50%, -50%) rotate(45deg); }
+
+      span.tercer-mod { transform: translate(-50%, -50%) rotate(-45deg); }
     `
   }
 
   connectedCallback () {
+    this.state = false
     this.render()
+    this.addEventListener('click', () => this.infoStatus(this.state))
+  }
+
+  infoStatus (state) {
+    console.log(this.shadowRoot.children[1].classList)
+    if (state === false) {
+      this.state = true
+      this.shadowRoot.children[1].classList.add('primer-mod')
+      this.shadowRoot.children[2].classList.add('segundo-mod')
+      this.shadowRoot.children[3].classList.add('tercer-mod')
+      this.shadowRoot.children[4].classList.add('cuarto-mod')
+    } else {
+      this.state = false
+      this.shadowRoot.children[1].classList.remove('primer-mod')
+      this.shadowRoot.children[2].classList.remove('segundo-mod')
+      this.shadowRoot.children[3].classList.remove('tercer-mod')
+      this.shadowRoot.children[4].classList.remove('cuarto-mod')
+    }
+
+    const newEvent = new CustomEvent('isOpen', { detail: this.state, composed: true, bubbles: true })
+    this.dispatchEvent(newEvent)
   }
 
   render () {
--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-import './components/FloatOption.js'
 import './components/ButtonHamburger.js'
+import './components/FloatOption.js'
 
+const nav = document.querySelector('.nav')
 const navTitle = document.querySelector('.nav__title')
-const hamburger = document.querySelector('.nav__btn')
 const floatOption = document.querySelector('.floatOption')
 
+const textTitle = navTitle.innerText
 let time = 0
-let textTitle = navTitle.innerText
 function printTextTitleNav () {
   if (time < textTitle.length) {
     navTitle.innerHTML += textTitle.charAt(time)
@@ -21,20 +21,16 @@ function printContentBoxFloat () {
   floatOption.innerHTML = '<float-option></float-option>'
 }
 
-let isOpen = false
-hamburger.addEventListener('click', () => {
-  if (!isOpen) {
-    isOpen = true
-    hamburger.classList.add('hamburgerOpen')
+nav.addEventListener('isOpen', (e) => {
+  const data = e.detail
+
+  if (data === true) {
     floatOption.classList.add('openFloat')
     setTimeout(printContentBoxFloat, 400)
   } else {
-    isOpen = false
-    hamburger.classList.remove('hamburgerOpen')
     floatOption.classList.remove('openFloat')
     floatOption.innerHTML = ''
     time = 0
-    textTitle = navTitle.innerText
     navTitle.innerHTML = ''
     setTimeout(printTextTitleNav, 400)
   }
